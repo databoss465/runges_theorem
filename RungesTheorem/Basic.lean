4b3fe37ff6d0971f0b6c0a64aa5e06b03468eb58
Databoss465 <dattabhasvan@iisc.ac.in>
2025-03-18 15:32:49 +0530

    Added statement of lemma on equality of open sets

--- RungesTheorem/Basic.lean
+++ RungesTheorem/Basic.lean
@@ -1,4 +1,16 @@
 import Mathlib.Analysis.Complex.Basic
 import Mathlib.Topology.Basic
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.CategoryTheory.ConnectedComponents
+
+-- open TopologicalSpace
 
 #check Complex
+variable {U V : Set ℂ}
+variable {z : ℂ}(h : z ∈ U)
+#check IsOpen U
+#check connectedComponentIn U z ∩ V ≠ ∅
+
+lemma open_subset_intersesecting_comp_eq {U V : Set ℂ} (h₁ : IsOpen U ∧ IsOpen V) (h₂ : V ⊆ U) (h₃: frontier V ∩ U = ∅)
+(h₄: ∀ z ∈ U, connectedComponentIn U z ∩ V ≠ ∅) : U = V := by
+  sorry
